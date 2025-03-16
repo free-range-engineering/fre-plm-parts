a12cd750cd4713bfdc2c5e8d450a241122658481
Marcus Svensson <marcus@freerangeengineering.se>
2025-03-16 20:32:54 +0100

    feat: Added script to help adding components from JLCPCB. (WIP)

--- tools/code_calc.py
+++ tools/code_calc.py
@@ -4,70 +4,75 @@ import re
 import argparse
 import os
 
-argparser =  argparse.ArgumentParser(description='Converts Farads (or Ohm) values into variation codes, outputting the code as the last line of output')
-argparser.add_argument('value', nargs=1, help="value including 'M', 'k', 'm', '',  'u', 'n' and 'p'")
-argparser.add_argument('-r', '--resistor', action='store_true', help="Switches to resistor mode")
-argparser.add_argument('-u', '--micro', action='store_true', help="Switches to resistor mode to micro ohms (for shunts)")
+unit_decode = {'M': 6, 'k': 3, '': 0, 'm': -3, 'u': -6, 'n': -9, 'p': -12}
 
-unit_decode = {'M': 6, 'k': 3, '': 0, 'm':- 3, 'u': -6, 'n': -9, 'p': -12}
-base_unit = -12
-base_text = "pF"
-extra_unit = "f|F"
+def parse_value(value, resistor=False, micro=False):
+    base_unit = -12
+    base_text = "pF"
+    extra_unit = "f|F"
 
-args = argparser.parse_args()
-value = args.value[0]
+    if resistor or micro:
+        if micro:
+            base_unit = -6
+            base_text = "uOhm"
+        else:
+            base_unit = 0
+            base_text = "Ohm"
+        extra_unit = "Ohm|ohm|R|r|Ω"
 
-if args.resistor or args.micro:
-    if args.micro:
-        base_unit = -6
-        base_text = "uOhm"
-    else:
-        base_unit = 0
-        base_text = "Ohm"
-    extra_unit = "Ohm|ohm|R|r"
+    result = re.search(r"^\s*([0-9]+)[,.]?([0-9]*)\s*([Mkmunp]?)\s*(" + extra_unit + r")?\s*$", value)
+    if result is None:
+        raise ValueError(f"Unable to understand {value} as a value")
 
-result = re.search("^\s*([0-9]+)[,.]?([0-9]*)\s*([Mkmunp]?)\s*("+extra_unit+")?\s*$", value)
-if (result == None):
-    print("Unable to understand {:s} as a value".format(value))
-    os._exit(1)
+    whole = result.groups()[0]
+    decimal = result.groups()[1] or ""
+    unit = result.groups()[2] or ""
+    decimal = decimal.rstrip('0')  # Ignore decimal trailing 0's, whole trailing is handled by the multiplier
 
-whole   = result.groups()[0]
-decimal = result.groups()[1]
-unit    = result.groups()[2]
-if decimal == None:
-    decimal = ""
-if unit == None:
-    unit = ""
-decimal = decimal.rstrip('0') #Ignore decimal trailing 0's, whole trailing is handled by the multiplier
+    code = whole + decimal
+    suffix_zeros = unit_decode[unit] - base_unit - len(decimal)
+    if suffix_zeros < 0:
+        raise ValueError(f"{value} has a too small part to encode in an integer base of {base_text}")
 
-code = whole+decimal
+    suffix_string = "".ljust(suffix_zeros, "0")
+    as_base = code + suffix_string
+    as_base = as_base.lstrip('0')  # Ignore prefixed zeros
 
-suffix_zeros = unit_decode[unit] - base_unit - len(decimal)
-if suffix_zeros < 0:
-    print("{:s} has a too small part to encode in a integer base of {:s}".format(value, base_text))
-    os._exit(1)
-suffix_string = "".ljust(suffix_zeros, "0")
+    multiplier = len(as_base) - len(as_base.rstrip('0'))
+    if multiplier > 9:
+        multiplier = 9
 
-as_base = code+suffix_string
-as_base = as_base.lstrip('0') #Ignore prefixed zeros, for instance 0.111uF that would otherwise have to many digits
+    code = as_base
+    if multiplier > 0:
+        code = code[0:-multiplier]
+    code = code.rjust(3, '0')
 
-multiplier = len(as_base)-len(as_base.rstrip('0'))
-if multiplier > 9:
-    multiplier = 9
+    if len(code) > 3:
+        stripped_code = code.rstrip('0')
+        if len(stripped_code) > 3:
+            raise ValueError(f"Too many significant digits in {value}")
+        else:
+            raise ValueError(f"{value} too large to encode in {base_text}")
 
-code = as_base
-if multiplier > 0:
-    code = code[0:-multiplier]
-code = code.rjust(3, '0')
+    return code, multiplier, as_base, base_text
 
-if len(code) > 3:
-    stripped_code = code.rstrip('0')
-    if len(stripped_code) > 3:
-        print("Too many significant digits in {:s}".format(value))
-    else:
-        print("{:s} too large to encode in {:s}".format(value, base_text))
-    os._exit(1)
+def main():
+    argparser = argparse.ArgumentParser(description='Converts Farads (or Ohm) values into variation codes, outputting the code as the last line of output')
+    argparser.add_argument('value', nargs=1, help="value including 'M', 'k', 'm', '',  'u', 'n' and 'p'")
+    argparser.add_argument('-r', '--resistor', action='store_true', help="Switches to resistor mode")
+    argparser.add_argument('-u', '--micro', action='store_true', help="Switches to resistor mode to micro ohms (for shunts)")
 
-print ("Read {:s} as {:s} giving a code of:".format(value, as_base + base_text))
-print ("{:s}{:d}".format(code, multiplier))
+    args = argparser.parse_args()
+    value = args.value[0]
+
+    try:
+        code, multiplier, as_base, base_text = parse_value(value, args.resistor, args.micro)
+        print(f"Read {value} as {as_base + base_text} giving a code of:")
+        print(f"{code}{multiplier}")
+    except ValueError as e:
+        print(e)
+        os._exit(1)
+
+if __name__ == "__main__":
+    main()
 
---
+++ tools/jpn_import.py
@@ -0,0 +1,265 @@
+import argparse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+from enum import Enum
+from code_calc import parse_value
+from fractions import Fraction
+
+
+class ComponentCategory(Enum):
+    ANA = "ANA"         # op-amps, comparators, A/D, D/A
+    CAP = "CAP"         # capacitors
+    CON = "CON"         # connectors
+    CPD = "CPD"         # Circuit protection devices
+    DIO = "DIO"         # diodes
+    IND = "IND"         # inductors, transformers
+    ICS = "ICS"         # integrated circuits
+    MPU = "MPU"         # SOC, SOM, SBC, etc.
+    MCU = "MCU"         # Microcontrolleres, modules, etc.
+    OPT = "OPT"         # Optical, couplers, phototransistor, etc.
+    OSC = "OSC"         # oscillators, Crystals
+    PWR = "PWR"         # relays, etc
+    RFM = "RFM"         # RF modules, ICs, and related components
+    REG = "REG"         # regulators
+    RES = "RES"         # resistors
+    SEN = "SEN"         # sensors
+    SWI = "SWI"         # switch
+    XTR = "XTR"         # transistors, FETs
+
+
+category_map = {
+    "Capacitors/Multilayer Ceramic Capacitors MLCC - SMD/SMT": ComponentCategory.CAP,
+    "Resistors/Chip Resistor - Surface Mount": ComponentCategory.RES,
+    "Connectors/Pin Headers": ComponentCategory.CON,
+    "Interface/CAN Transceivers": ComponentCategory.ICS,
+    "Transistors/Thyristors/Bipolar (BJT)": ComponentCategory.XTR,
+    "Transistors/Thyristors/MOSFETs": ComponentCategory.XTR,
+    "Power Management (PMIC)/Voltage Regulators - Linear, Low Drop Out (LDO) Regulators": ComponentCategory.REG
+}
+
+
+def res_package(package):
+    if "Plugin" in package:
+        return "Resistor_THT:R_Axial_DIN0207_L6.3mm_D2.5mm_P7.62mm_Horizontal"
+    if package == "0402":
+        return "Resistor_SMD:C_0402_1005Metric"
+    if package == "0603":
+        return "Resistor_SMD:C_0603_1608Metric"
+    if package == "0805":
+        return "Resistor_SMD:C_0805_2012Metric"
+    if package == "1206":
+        return "Resistor_SMD:C_1206_3216Metric"
+    return f"UNKNOWN:{package}"
+
+
+def cap_package(package):
+    if package == "0402":
+        return "Capacitor_SMD:C_0402_1005Metric"
+    if package == "0603":
+        return "Capacitor_SMD:C_0603_1608Metric"
+    if package == "0805":
+        return "Capacitor_SMD:C_0805_2012Metric"
+    if package == "1206":
+        return "Capacitor_SMD:C_1206_3216Metric"
+
+    return f"UNKNOWN:{package}"
+
+
+def format_to_row(data: dict):
+    try:
+
+        category = category_map[data["Category"]]
+        if category == ComponentCategory.CAP:
+            code, multiplier, as_base, base_text = parse_value(
+                data["Capacitance"], False, False)
+            return ",".join([f"CAP-XXXX-{code}{multiplier}",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            "Device:C_Small",
+                             cap_package(data["Package"]),
+                             data["Capacitance"],
+                             data["Voltage Rating"],
+                             data["Temperature Coefficient"],  # "Material"
+                             data["Tolerance"],
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+
+        if category == ComponentCategory.RES:
+            code, multiplier, as_base, base_text = parse_value(
+                data["Resistance"], True, False)
+            return ",".join([f"RES-XXXX-{code}{multiplier}",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            "Device:R_Small",
+                             res_package(data["Package"]),
+                             data["Resistance"],
+                             data["Voltage-Supply(Max)"],
+                             str(float(
+                                 Fraction(data["Power(Watts)"].replace("W", "")))),
+                             data["Tolerance"],
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+
+        if category == ComponentCategory.CON:
+            return ",".join(["CON-XXXX-XXXX",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            "UNKNOWN",
+                             "UNKNOWN",
+                             "UNKNOWN",
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+
+        if category == ComponentCategory.ICS:
+            return ",".join(["ICS-XXXX-XXXX",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            "UNKNOWN",
+                             data["Package"],
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+
+        if category == ComponentCategory.XTR:
+            return ",".join(["XTR-XXXX-XXXX",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            "UNKNOWN",
+                             data["Package"],
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+        if category == ComponentCategory.REG:
+            return ",".join(["REG-XXXX-XXXX",
+                            data["MPN"],
+                            data["Manufacturer"].replace(",", " "),
+                            data["Description"].replace(",", " "),
+                            data["Output Voltage"],
+                            data["Output Current"],
+                            "UNKNOWN",
+                             data["Package"],
+                             data["Datasheet"],
+                             data["JPN"],
+                             data["JPT"]])
+    except KeyError as e:
+        print(data)
+        raise e
+
+
+def fetch_info_responce(driver, jpn):
+
+    results = dict()
+    url = f"https://www.lcsc.com/search?q={jpn}"
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    static_categories = ["category_id",
+                         "manufacturer_id",
+                         "package_id"]
+    for sc in static_categories:
+        category = soup.find('td', id=sc)
+        category_text = category.get_text(strip=True)
+        value = category.find_next_sibling('td').get_text(strip=True)
+        results.update({category_text: value})
+
+    for index in range(0, 10):
+        category = soup.find('td', id=f"paramsItem{index}")
+        if not category:
+            break
+        category_text = category.get_text(strip=True)
+        value = category.find_next_sibling('td').get_text(strip=True)
+        results.update({category_text: value})
+    try:
+
+        datasheet_td = soup.find('td', id="datasheet_id")
+        datasheet_td_text = datasheet_td.get_text(strip=True)
+        datasheet_href = datasheet_td.find_next_sibling(
+            'td').find("a").attrs["href"]
+        results.update({datasheet_td_text: datasheet_href})
+    except:
+        results.update({"Datasheet": "N/A"})
+
+    MPN_ts = soup.find(
+        'td', string=lambda text: text and "Mfr. Part #" in text)
+    value = MPN_ts.find_next_sibling('td').get_text(strip=True)
+    results.update({"MPN": value})
+
+    description_td = soup.find(
+        'td', string=lambda text: text and "Description" in text)
+    value = description_td.find_next_sibling('td').get_text(strip=True)
+    results.update({"Description": value})
+    return results
+
+
+def fetch_jpt_url(driver, jpn):
+    url = f"https://jlcpcb.com/parts/componentSearch?searchTxt={jpn}"
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    jpt = soup.select_one(
+        "span[class*='text-12 text-666666 ml-4']")
+    jpt_recomended = jpt.find_previous_sibling("svg")
+    jpt = jpt.text.strip()
+    if jpt_recomended and jpt == "Extended":
+        jpt = "Preferred"
+
+    return {"JPT": jpt, "JPN": jpn}
+
+
+if __name__ == "__main__":
+    print("JPN Import Start")
+    parser = argparse.ArgumentParser(
+        description='Fetch component data from JLCPCB.')
+    parser.add_argument('JPN', type=str, nargs='+',
+                        help='The list of JPNs to search for')
+    parser.add_argument('--only-basic', action='store_true',
+                        help='Fetch only basic information', default=False)
+    args = parser.parse_args()
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+        driver = webdriver.Chrome(service=Service(
+            ChromeDriverManager().install()), options=chrome_options)
+
+        for jpn in args.JPN:
+            result = {**fetch_jpt_url(driver, jpn),
+                      **fetch_info_responce(driver, jpn)}
+            if args.only_basic and result["JPT"] != "Basic" and result["JPT"] != "Preferred":
+                print(
+                    f"Skipping: {jpn} only basic allowed, found:{result['JPT']}")
+                print(result)
+                continue
+
+            row = format_to_row(result)
+
+            category = category_map[result["Category"]]
+            with open(f"../database/g-{category.value.lower()}.csv", "a", encoding="utf-8") as file:
+                file.write("\n"+row)
+
+            if row:
+                print(row)
+            else:
+                print("Unknown result")
+                print(result)
+
+    except Exception as e:
+        print(f"An error occurred: {e.with_traceback()}")
+    finally:
+        # pass
+        driver.quit()
